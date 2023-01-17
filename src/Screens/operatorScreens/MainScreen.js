08f221a70619815b06ca1245d1029f9f06ad35d2
unknown <akashnair032@gmail.com>
2023-01-17 07:49:35 +0530

    changed initial route to login

--- src/Screens/operatorScreens/MainScreen.js
+++ src/Screens/operatorScreens/MainScreen.js
@@ -1,3 +1,4 @@
+/* eslint-disable prettier/prettier */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 import {View, Text, SafeAreaView, Pressable} from 'react-native';
